d845b188102486ccb3f55c1d0c601d91fa0d444d
Kylo22 <91325053+Kylo22@users.noreply.github.com>
2023-11-13 20:04:33 -0600

    Epress website,static HTML only

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,12 @@
-/* GET travel view */
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
-   };
-   
-   module.exports = {
+    res.render('travel', {title: "Travlr Getaways - Travel", trips});
+};
+
+module.exports = {
     travel
-   };
+};
    
